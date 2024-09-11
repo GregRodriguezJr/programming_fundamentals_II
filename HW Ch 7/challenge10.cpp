4c32105a7abba9432788f674d187b9364bdc9446
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2024-09-11 11:57:42 -0500

    completed lab PC10

--- HW Ch 7/challenge10.cpp
+++ HW Ch 7/challenge10.cpp
@@ -1,50 +1,38 @@
-//
-// Created by Greg Rodriguez on 9/10/24.
-//
-// print total number of correctly and incorrectly answered questions and which numbers were wrong
+// Ch. 7 PC #10 Driver's License Exam
+// Created by Greg Rodriguez
+// COSC 1437
+// 9/11/24
+// Purpose: To check a students answers for an exam.
+// Input: User inputs letter choices from students exam from keyboard cin. Maximum input is 20 letters and only A, B, C, or D.
+// Output: Results are printed using cout with details about the test scores.
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-const int MAXSIZE = 2;
+const int MAXSIZE = 20;
 
-// Creates a new data type array to hold char values
-typedef char ScantronType[MAXSIZE];
+typedef char ScantronType[MAXSIZE];     // Creates a new data type array to hold char values
 
-// This function gets the students answers from user
-void getAnswers(ScantronType, int);
-
-// Function to compare arrays and store results in new array
-void checkAnswers(ScantronType, ScantronType, ScantronType, int);
-
-// Function to print results to terminal
-void printResults(ScantronType, int);
+void getAnswers(ScantronType, int);     // This function gets the students answers from user
+void checkAnswers(ScantronType, ScantronType, ScantronType, int);   // Function to compare arrays and store results in new array
+void printResults(const ScantronType, int);   // Function to print results to terminal
+void printWrongAnswers(const ScantronType, int);   // Function to print wrong answers to the terminal
 
 int main() {
 
     // Array to hold correct answers
-    ScantronType correctAnswers = {'A','B'};
-//            {'A', 'D', 'B', 'B', 'C', 'B', 'A', 'B', 'C', 'D',
-//                               'A', 'C', 'D', 'B', 'D', 'C', 'C', 'A', 'D', 'B'};
-    // Array to hold student answers
-    ScantronType studentAnswers;
+    ScantronType correctAnswers = {'A', 'D', 'B', 'B', 'C', 'B', 'A', 'B', 'C', 'D', 'A', 'C', 'D', 'B', 'D', 'C', 'C', 'A', 'D', 'B'};
+    ScantronType studentAnswers;    // Array to hold student answers
+    ScantronType resultAnswers;     // Array to hold student wrong answers
 
-    // Array to hold student wrong answers
-    ScantronType resultAnswers;
+    cout << setw(48) << "Welcome to Driver's License Exam\n";   // Prompt user
+    cout << setw(50) << "Valid entries are capital A,B,C or D.\n\n";
 
-    // Prompt user
-    cout << setw(48) << "Welcome to Driver's License Exam\n";
-    cout << setw(50) << "Valid entries are capital A,B,C or D.\n";
-
-    // Function call to get students answers
-    getAnswers(studentAnswers, MAXSIZE);
-
-    // Function call to compare arrays
-    checkAnswers(correctAnswers, studentAnswers, resultAnswers, MAXSIZE);
-
-    // Function call to print results
-    printResults(resultAnswers, MAXSIZE);
+    getAnswers(studentAnswers, MAXSIZE);    // Function call to get students answers
+    checkAnswers(correctAnswers, studentAnswers, resultAnswers, MAXSIZE);   // Function call to compare arrays
+    printResults(resultAnswers, MAXSIZE);   // Function call to print results
+    printWrongAnswers(resultAnswers, MAXSIZE);   // Function call to print results
 
     return 0;
 }
@@ -66,7 +54,7 @@ void getAnswers(ScantronType studentAnswers, int arraySize){
         isValid = false;
 
         while(!isValid) {
-            cout << "\nEnter student's answer letter for Question " << i+1 << " : ";
+            cout << "Enter student's answer letter for Question " << i+1 << " : ";
             cin >> gradeLetter;
 
             if(gradeLetter == 'A' || gradeLetter == 'B' ||gradeLetter == 'C' || gradeLetter == 'D'){
@@ -88,6 +76,7 @@ void getAnswers(ScantronType studentAnswers, int arraySize){
 //
 //***********************************************************************
 void checkAnswers(ScantronType correctAnswers,ScantronType studentAnswers, ScantronType resultAnswers, int arraySize){
+
     for(int i = 0; i < arraySize; i++){
         if(studentAnswers[i] == correctAnswers[i]){
             resultAnswers[i] = 'T';
@@ -105,7 +94,7 @@ void checkAnswers(ScantronType correctAnswers,ScantronType studentAnswers, Scant
 //  data returned: None, the function calculates passing grade prints results.
 //
 //***********************************************************************
-void printResults(ScantronType resultAnswers, int arraySize){
+void printResults(const ScantronType resultAnswers, int arraySize){
     int numCorrect = 0;
     int passScore;
 
@@ -121,8 +110,33 @@ void printResults(ScantronType resultAnswers, int arraySize){
     cout << "Number of incorrect answers: " << arraySize - numCorrect << endl;
 
     if(passScore >= 60) {
-        cout << "Student Passed" << endl;
+        cout << "Student Passed\n" << endl;
     } else {
         cout << "Student Failed\n" << endl;
     }
+}
+
+//***********************************************************************
+//                              printWrongAnswers
+//
+//  task: To print incorrect answers and display results.
+//  data in: ResultAnswers and array size is taken in as an arguments.
+//  data returned: None, the function prints wrong questions and "none" if they're all right.
+//
+//***********************************************************************
+void printWrongAnswers(const ScantronType resultAnswers, int arraySize){
+    int numWrong = 0;
+    cout << "Incorrect answered questions \n";
+
+    for (int i = 0; i < arraySize; ++i) {
+        if(resultAnswers[i] == 'F'){
+            cout << i + 1 << "  ";
+            numWrong++;
+        }
+    }
+
+    if(numWrong == 0) {
+        cout << "None";
+    }
+    cout << endl;
 }
